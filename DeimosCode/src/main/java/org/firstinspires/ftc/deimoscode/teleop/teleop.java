8f4a9ac0ff6e92abd6dbb007ef87bb82e848f301
Sebastian Erives <serivesmejia@gmail.com>
2021-11-04 16:12:12 -0700

    deimos o algo asi

---
+++ DeimosCode/src/main/java/org/firstinspires/ftc/deimoscode/teleop/teleop.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.deimoscode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp( name = "TeleOp", group = "Chido")
+public class TeleOp extends LinearOpMode {
+
+    private DcMotor motorleft;
